758672aa3c3664aab63c95c72581f67ac55ea499
hung hoang <hung.xamk@gmail.com>
2023-05-04 10:39:42 +0300

    add input validation for OpeningHour

--- cinemaApi/DTOs/CinemaDto.cs
+++ cinemaApi/DTOs/CinemaDto.cs
@@ -1,4 +1,6 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace cinemaApi.DTOs
 {
     public class CinemaDto
@@ -6,6 +8,8 @@ namespace cinemaApi.DTOs
         public class CinemaBaseDto
         {
             public string Name { get; set; } = null!;
+
+            [Range(12, 23, ErrorMessage = "The opening hour must be between 12 and 23.")]
             public int OpeningHour { get; set; }
             public int ClosingHour { get; set; }
             public int ShowDuration { get; set; }
--- cinemaApi/Models/Cinema.cs
+++ cinemaApi/Models/Cinema.cs
@@ -4,8 +4,6 @@ namespace cinemaApi.Models
     public class Cinema : BaseModel
     {
         public string Name { get; set; } = null!;
-
-        [Range(12, 23, ErrorMessage = "The opening hour must be between 12 and 23.")]
         public int OpeningHour { get; set; }
         public int ClosingHour { get; set; }
         public int ShowDuration { get; set; }
